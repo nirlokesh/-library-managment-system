161060e0e999b89aa61ce442e73b245b6f4eb538
nirlokesh <Nirlokesh@8>
2022-01-03 14:55:15 +0530

    DB scripts and required data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS Book;
+
+CREATE TABLE Book (
+  ISBN varchar(13) PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(100) NOT NULL,
+  count INT DEFAULT 1
+);
+
+INSERT INTO Book (name, description, count) VALUES
+  ('101','All About Java', 'Written by John T. Includes deep understanding of java with examples.', 40),
+  ('102','Spring 2.0', 'Written by David H. With greate expalination about how spring works.', 30),
+  ('103','HTML&CSS', 'Written by Phillip Q. Learn to create and design web pages with html and CSS.', 40),
+  ('104','Spring boot', 'Written by Holger M. With greate expalination about how spring boot works.', 30),
+  ('105','Angular 2+', 'Written by Heiko G. All new angular concepts and examples.', 40),
+  ('106','.Net', 'Written by Reduane H. Learn to build APIs with .Net.', 30),
+  ('107','Head first JSP', 'Written by Alakija. All you know about HTML is here.', 50),
+  ('108','C & C++', 'Written by Sameer. Explore with programming with c and c++.', 50);
+  
+CREATE TABLE BorrowList (
+  id INT AUTO_INCREMENT,
+  username VARCHAR(250) NOT NULL,
+  ISBN VARCHAR(250) NOT NULL,
+  booking_date date NOT NULL,
+  return_date date NOT NULL,
+  PRIMARY KEY (username, ISBN)
+);
+
+DROP TABLE IF EXISTS borrower;
+
+CREATE TABLE borrower (
+  card_id INT AUTO_INCREMENT PRIMARY KEY,
+  username varchar(100) NOT NULL,
+  phone varchar(20) NOT NULL,
+);
+
+INSERT INTO Book (username, phone) VALUES
+  ('hexad', 490909090);
